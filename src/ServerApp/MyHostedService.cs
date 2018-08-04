a5229aff20baf7c16f40cf7d65bf49c1da78a6ef
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2018-08-04 11:01:07 +0200

    Using Microsoft.Extensions.Hosting as described here: https://stackoverflow.com/a/49233560/66629

---
+++ src/ServerApp/MyHostedService.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ServerApp
+{
+    public class MyHostedService : IHostedService
+    {
+        private readonly ILogger<MyHostedService> _logger;
+
+        public MyHostedService(ILogger<MyHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Service started");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Service stopped");
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/ServerApp/Program.cs
+++ src/ServerApp/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ServerApp
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Running with args: " + string.Join(", ", args));
+            await Console.Out.WriteLineAsync("Running with args: " + string.Join(", ", args));
+
+            var hostBuilder = new HostBuilder()
+                .ConfigureLogging(logging => {
+                    // See https://msdn.microsoft.com/en-us/magazine/mt694089.aspx
+
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                    logging.AddConsole();
+                })
+                .ConfigureServices(services => {
+                    services.AddSingleton<IHostedService, MyHostedService>();
+                });
+
+            await hostBuilder.RunConsoleAsync();
+            
+            await Console.Out.WriteLineAsync("Done.");
         }
     }
 }
