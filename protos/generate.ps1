10a447cd9fb2a5d64f55507066ad4577cbe0ed15
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2018-08-07 21:44:12 +0200

    Added sample proto and generation script

---
+++ protos/generate.ps1
@@ -0,0 +1,44 @@
+﻿$ErrorActionPreference = "Stop"
+
+function Install-NuGetTool($Id, $Version) {
+    $OutputDirectory = "$PSScriptRoot\packages"
+    $NuGetApp = (Get-Command -Name nuget -CommandType Application).Source
+    $NuGetArgs = @(
+        "install", $Id,
+        "-Version", $Version,
+        "-Verbosity", "quiet",
+        "-ForceEnglishOutput",
+        "-OutputDirectory", $OutputDirectory
+    )
+    & $NuGetApp $NuGetArgs
+    if ($LASTEXITCODE -ne 0) {
+        throw "NuGet invocation returned $LASTEXITCODE"
+    }
+
+    return "$OutputDirectory\$Id.$Version"
+}
+
+$GrpcTools = Install-NuGetTool -Id "Grpc.Tools" -Version "1.14.0"
+$ProtoApp = "$GrpcTools\tools\windows_x64\protoc.exe"
+$GrpcPluginApp = "$GrpcTools\tools\windows_x64\grpc_csharp_plugin.exe"
+
+$ProtoFiles = Get-ChildItem -Path $PSScriptRoot -Filter "*.proto" -Recurse
+foreach ($ProtoFile in $ProtoFiles) {
+    $ProtoDir = $ProtoFile.Directory.FullName
+    $ProtoFile = $ProtoFile.FullName
+
+    Write-Output "Generating: $ProtoFile"
+
+    $ProtoArgs = @(
+        "--proto_path=$ProtoDir",
+        "--csharp_out=$ProtoDir",
+        "--plugin=protoc-gen-grpc=$GrpcPluginApp",
+        "--grpc_out=$ProtoDir",
+        $ProtoFile
+    )
+
+    & $ProtoApp $ProtoArgs
+    if ($LASTEXITCODE -ne 0) {
+        throw "Proto invocation returned $LASTEXITCODE"
+    }
+}
