5fb413cd75fd52afb51e6bd4422913755a72af47
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2019-03-16 12:49:28 +0100

    Switched from netcoreapp2.1 to netcoreapp2.2, updated NuGet packages

--- src/MyCompany.MyStack.MyRestApp/Program.cs
+++ src/MyCompany.MyStack.MyRestApp/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System.IO;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace MyCompany.MyStack.MyRestApp
@@ -22,10 +21,10 @@ namespace MyCompany.MyStack.MyRestApp
 
         public static IWebHostBuilder CreateHostBuilder()
         {
-            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-2.1
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host
             // The Generic Host is new in ASP.NET Core 2.1 and isn't suitable for web hosting scenarios. For web hosting scenarios, use the Web Host.
             // The Generic Host is under development to replace the Web Host in a future release and act as the primary host API in both HTTP and non-HTTP scenarios.
-            // To run background task, see https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/hosted-services?view=aspnetcore-2.1
+            // To run background task, see https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/hosted-services
             return new WebHostBuilder()
                 .UseKestrel(options => {
                     options.AddServerHeader = false;
