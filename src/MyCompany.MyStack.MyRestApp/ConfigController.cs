82b0b10c298f5271b1f49e41891d180ccea2ffd6
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2019-04-07 11:39:41 +0200

    Implemented config.html web page

--- src/MyCompany.MyStack.MyRestApp/ConfigController.cs
+++ src/MyCompany.MyStack.MyRestApp/ConfigController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace MyCompany.MyStack.MyRestApp
 {
@@ -19,23 +21,38 @@ namespace MyCompany.MyStack.MyRestApp
             _appConfig = appConfig;
         }
 
-        [HttpGet("api/v1/config")]
-        public async Task<ActionResult> GetConfigAsync([FromQuery(Name = "module-instance-id")] string moduleInstanceId)
+        [HttpGet("api/v1/config/{moduleInstanceId}")]
+        public async Task<ActionResult> GetConfigAsync(string moduleInstanceId)
         {
-            if (string.IsNullOrWhiteSpace(moduleInstanceId))
-            {
-                return BadRequest(new { Error = "Missing required query parameter: module-instance-id" });
-            }
             var cfgPath = Path.Combine(_appConfig.DataDir, moduleInstanceId + ".json");
             if (System.IO.File.Exists(cfgPath))
             {
                 var json = await System.IO.File.ReadAllTextAsync(cfgPath, Encoding.UTF8);
                 return Ok(JsonConvert.DeserializeObject(json));
             }
-            else
+
+            return Ok(new JObject());
+        }
+
+        [HttpPut("api/v1/config/{moduleInstanceId}")]
+        public async Task<ActionResult> PutConfigAsync(string moduleInstanceId, [FromBody] object body)
+        {
+            if (!ModelState.IsValid)
             {
-                return NotFound(new { Error = "No config found for " + moduleInstanceId });
+                return BadRequest(ModelState);
             }
+            var cfgPath = Path.Combine(_appConfig.DataDir, moduleInstanceId + ".json");
+            var json = JsonConvert.SerializeObject(body, Formatting.Indented);
+            await System.IO.File.WriteAllTextAsync(cfgPath, json, Encoding.UTF8);
+            return Ok();
+        }
+
+        [HttpGet("config/{moduleInstanceId}")]
+        public ActionResult GetConfigUiAsync()
+        {
+            // ~/ match {Content Root}/wwwroot
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/index?view=aspnetcore-2.2&tabs=windows#web-root
+            return File("~/config.html", "text/html");
         }
     }
 }
